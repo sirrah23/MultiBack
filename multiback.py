7e33ee62c8fd2d9f6472573dc4bd31771e89c4d9
sirrah23 <hseth93@gmail.com>
2018-06-28 00:22:55 -0400

    Initial commit (skeleton)

---
+++ multiback.py
@@ -0,0 +1,20 @@
+def is_dir(dirs):
+    pass
+
+def read_config(cfg):
+    pass
+
+def backup_file(src, dests):
+    pass
+
+def backup_files(srcs, dests):
+    pass
+
+def fname_timestamp(fname):
+    pass
+
+def rename_file(old_fname, new_fname):
+    pass
+
+def copy_file(src, dest):
+    pass
